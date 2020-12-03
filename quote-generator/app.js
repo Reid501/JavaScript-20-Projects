4de9e083de418af2c2210e6d879ac6d5f46d5ff8
Alex Reid <alexatrs@hotmail.co.uk>
2020-12-03 15:06:37 +0000

    Code added to project

---
+++ quote-generator/app.js
@@ -0,0 +1,17 @@
+// Get Quote from API
+
+async function getQuote() {
+    const proxyUrl = 'https://protected-ocean-89794.herokuapp.com/';
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        console.log(data);
+    } catch(error) {
+        getQuote();
+        console.log(`Whoops, no quote`, error);
+    }
+}
+
+// On Load
+getQuote();
